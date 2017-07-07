9c156f7225281c143412a523af49a1a3c7ff4d2e
NandBa <nandinibanerjee14@gmail.com>
2017-07-08 04:08:04 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,3 +1,4 @@
+#Reading data
 data<-read.table("household_power_consumption.txt", header=TRUE, sep=";",
                  na.strings = "?")
 
@@ -7,7 +8,7 @@ data$Time=format(strptime(data$Time,format="%H:%M:%S"),"%H:%M:%S")
 data$DateTime<-paste(data$Date, data$Time, sep=" ")
 data$DateTime<-strptime(data$DateTime, format="%Y-%m-%d %H:%M:%S")
 
-
+#Plotting Graph
 png(file="plot3.png",width = 480,height = 480)
 plot(data$DateTime,data$Sub_metering_1, 
      type = "l",ylab = "Energy sub metering", xlab = "",lwd=2.5)
